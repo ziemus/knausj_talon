c703937ef7bc1f80114c1ed59d3cffb069f51002
Jeff Knaus <knaus.jeff@gmail.com>
2022-03-26 23:25:47 -0600

    Implement easy window switcher support "properly"

---
+++ apps/platforms/win/easy_window_switcher/easy_window_switcher.py
@@ -0,0 +1,29 @@
+from talon import Context, actions
+
+from talon import Context, actions, ui, Module
+
+mod = Module()
+ctx = Context()
+
+apps = mod.apps
+apps.easy_window_switcher = """
+os: windows
+and app.name: "Easy Window Switcher"
+os: windows
+and app.exe: wincycle.exe
+"""
+
+ctx = Context()
+ctx.matches = r"""
+os: windows
+user.running: Easy Window Switcher
+"""
+
+
+@ctx.action_class("app")
+class AppActions:
+    def window_next():
+        actions.key("alt-`")
+
+    def window_previous():
+        actions.key("alt-shift-`")
