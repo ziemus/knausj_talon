2dc93651fc0c38da0f0778778f40c39a6f4e28ed
Ziemowit Musiatowicz <musiatowiczz@gmail.com>
2025-09-23 17:02:17 +0200

    game mode: walking sim: improvements to foot switch camera and movement controls

--- core/modes/game_mode/controls/control_scheme/walking_sim.py
+++ core/modes/game_mode/controls/control_scheme/walking_sim.py
@@ -21,20 +21,19 @@ class FootSwitchActions:
         actions.user.switch_game_movement(True)
 
     def foot_switch_olympus_center_up(held: bool):
-        if held:
-            actions.user.switch_game_movement(False)
+        actions.user.switch_game_movement(False)
 
     def foot_switch_olympus_left_down():
-        actions.skip()
+        actions.user.game_turn_camera_start('left')
 
     def foot_switch_olympus_left_up(held: bool):
-        actions.user.game_turn_camera('left')
+        actions.user.game_turn_camera_stop(True, False)
 
     def foot_switch_olympus_right_down():
-        actions.skip()
+        actions.user.game_turn_camera_start('right', 2.0)
 
     def foot_switch_olympus_right_up(held: bool):
-        actions.user.game_turn_camera('right')
+        actions.user.game_turn_camera_stop(True, False)
 
     def foot_switch_pcsensor_center_down():
         actions.key("space:down")
